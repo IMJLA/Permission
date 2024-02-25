81f553d3f1052aff067f17fc359ad62b47b3e34e
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-02-25 12:18:31 -0800

    depth experiments

--- dist/0.0.290/Permission/Permission.psm1
+++ dist/0.0.290/Permission/Permission.psm1
@@ -2439,8 +2439,8 @@ function Out-PermissionReport {
                     { $Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile },
                     { $Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile },
                     { $Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile },
-                    { $Report | ConvertTo-Json -Compress -Depth 3 | Out-File -LiteralPath $ThisReportFile },
-                    { $Report | ConvertTo-Json -Compress -Depth 3 | Out-File -LiteralPath $ThisReportFile },
+                    { $Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile },
+                    { $Report | ConvertTo-Json -Compress -Depth 4 | Out-File -LiteralPath $ThisReportFile },
                     { <#$Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile#> },
                     { <#$Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile#> },
                     { <#$Report | ConvertTo-Json -Compress | Out-File -LiteralPath $ThisReportFile#> },
@@ -3505,6 +3505,7 @@ ForEach ($ThisFile in $CSharpFiles) {
 }
 
 Export-ModuleMember -Function @('Add-CacheItem','ConvertTo-ItemBlock','Expand-Permission','Expand-PermissionTarget','Find-ResolvedIDsWithAccess','Format-Permission','Format-TimeSpan','Get-CachedCimInstance','Get-CachedCimSession','Get-FolderAcl','Get-FolderColumnJson','Get-FolderPermissionsBlock','Get-FolderPermissionTableHeader','Get-FolderTableHeader','Get-HtmlBody','Get-HtmlReportFooter','Get-PermissionPrincipal','Get-PrtgXmlSensorOutput','Get-ReportDescription','Get-TimeZoneName','Get-UniqueServerFqdn','Initialize-Cache','Invoke-PermissionCommand','Out-PermissionReport','Remove-CachedCimSession','Resolve-AccessControlList','Resolve-Ace','Resolve-Acl','Resolve-Folder','Resolve-FormatParameter','Resolve-IdentityReferenceDomainDNS','Resolve-PermissionTarget','Select-ItemPermissionTableProperty','Select-ItemTableProperty','Select-UniquePrincipal')
+
 
 
 
--- src/Permission.psm1
+++ src/Permission.psm1
@@ -292,3 +292,4 @@ Export-ModuleMember -Function @('Add-CacheItem','ConvertTo-ItemBlock','Expand-Pe
 
 
 
+
