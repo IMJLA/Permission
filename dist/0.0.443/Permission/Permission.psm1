6cd1a6fdb6c91e9c092b0b17e214ca290db616fa
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-03-25 21:43:30 -0700

    update debug pause

--- dist/0.0.443/Permission/Permission.psm1
+++ dist/0.0.443/Permission/Permission.psm1
@@ -3688,7 +3688,7 @@ function Out-PermissionReport {
 
                             # Apply the report template to the generated HTML report body and description
                             $ReportParameters = $HtmlElements.ReportParameters
-                            pause
+
                             Write-LogMsg @LogParams -Text "New-BootstrapReport @ReportParameters"
                             New-BootstrapReport -Body $Body @ReportParameters
 
@@ -3697,7 +3697,7 @@ function Out-PermissionReport {
                             # Combine the header and table inside a Bootstrap div
                             Write-LogMsg @LogParams -Text "New-BootstrapDivWithHeading -HeadingText '$HtmlElements.SummaryTableHeader' -Content `$FormattedPermission.$Format`Group.Table"
                             $TableOfContents = New-BootstrapDivWithHeading -HeadingText $HtmlElements.SummaryTableHeader -Content $PermissionGroupings.Table -Class 'h-100 p-1 bg-light border rounded-3 table-responsive'
-
+                            pause
                             # Combine all the elements into a single string which will be the innerHtml of the <body> element of the report
                             Write-LogMsg @LogParams -Text "Get-HtmlBody -TableOfContents `$TableOfContents -HtmlFolderPermissions `$FormattedPermission.$Format.Div"
                             $Body = Get-HtmlBody -TableOfContents $TableOfContents @BodyParams
@@ -4858,6 +4858,7 @@ ForEach ($ThisFile in $CSharpFiles) {
 }
 
 Export-ModuleMember -Function @('Add-CacheItem','ConvertTo-ItemBlock','Expand-Permission','Expand-PermissionTarget','Find-ResolvedIDsWithAccess','Find-ServerFqdn','Format-Permission','Format-TimeSpan','Get-AccessControlList','Get-CachedCimInstance','Get-CachedCimSession','Get-FolderPermissionsBlockUNUSED','Get-PermissionPrincipal','Get-PrtgXmlSensorOutput','Get-TimeZoneName','Initialize-Cache','Invoke-PermissionCommand','Out-PermissionReport','Remove-CachedCimSession','Resolve-AccessControlList','Resolve-Ace','Resolve-Acl','Resolve-Folder','Resolve-FormatParameter','Resolve-IdentityReferenceDomainDNS','Resolve-PermissionTarget','Select-UniquePrincipal')
+
 
 
 
--- src/Permission.psm1
+++ src/Permission.psm1
@@ -445,3 +445,4 @@ Export-ModuleMember -Function @('Add-CacheItem','ConvertTo-ItemBlock','Expand-Pe
 
 
 
+
