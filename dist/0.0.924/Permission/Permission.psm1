947c1dd3f03bebcddf6490b06d650ac53512d16d
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-11-20 16:56:12 -0800

    bugfix cache usage

--- dist/0.0.924/Permission/Permission.psm1
+++ dist/0.0.924/Permission/Permission.psm1
@@ -3996,7 +3996,7 @@ function Find-ServerFqdn {
         $ThisFqdn = $null
     }
 
-    ForEach ($Value in $Cache['DomainByFqdn'].Value.Keys) {
+    ForEach ($Value in $Cache.Value['DomainByFqdn'].Value.Keys) {
         $UniqueValues[$Value] = $null
     }
 
@@ -6240,6 +6240,7 @@ ForEach ($ThisFile in $CSharpFiles) {
 }
 
 Export-ModuleMember -Function @('Add-CachedCimInstance','Add-CacheItem','Add-PermissionCacheItem','ConvertTo-ItemBlock','ConvertTo-PermissionFqdn','Expand-Permission','Expand-PermissionTarget','Find-CachedCimInstance','Find-ResolvedIDsWithAccess','Find-ServerFqdn','Format-Permission','Format-TimeSpan','Get-AccessControlList','Get-CachedCimInstance','Get-CachedCimSession','Get-PermissionPrincipal','Get-PermissionTrustedDomain','Get-PermissionWhoAmI','Get-TimeZoneName','Initialize-Cache','Invoke-PermissionAnalyzer','Invoke-PermissionCommand','New-PermissionCache','Out-Permission','Out-PermissionFile','Remove-CachedCimSession','Resolve-AccessControlList','Resolve-PermissionTarget','Select-PermissionPrincipal')
+
 
 
 
